a2cbe54d4f5bd496461e6fb678e951856f03c588
edrineasimwe <edrineasiimwe12@gmail.com>
2023-01-12 10:58:03 -0800

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,11 +17,11 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	array = (char *)malloc(sizeod(char) * size);
+	array = (char *)malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
 
-	for (i - 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		array[i] = c;
 
 	return (array);
