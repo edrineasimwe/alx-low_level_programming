c9cf40afec62c1cd86cb561492b53662fddc8882
edrineasimwe <edrineasiimwe12@gmail.com>
2023-01-17 05:35:55 -0800

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdhio.h>
+/**
+ * main - prints name of file.
+ * Return: Alwqys 0(success)
+ */
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,6 @@
+#ifndef ABS_H
+#define _FUNC_LIKE_MACRO_H
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#endif /*ABS_H*/
+
+
