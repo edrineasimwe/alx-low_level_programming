821c245b5a98759b9d64788d1bce29d611730517
edrineasimwe <edrineasiimwe12@gmail.com>
2023-01-21 04:19:14 -0800

    tas one

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include "dog.h"
-#include <stdio.h>
+#include <stdlib.h>
 /**
  * init_dog - function to initializevariable type dog
  * @d: struct to initialize
@@ -14,6 +14,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		return;
 
 	d->name = name;
-	d->name = age;
+	d->age = age;
 	d->owner = owner;
 }
