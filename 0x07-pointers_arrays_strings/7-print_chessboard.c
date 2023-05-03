dc63c6385b6ca0ae44ebd1454d6ed1eb3714b800
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-03 14:39:27 -0700

    A little game can't hurt anyone

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,20 +1,25 @@
 #include "main.h"
 #include <unistd.h>
-/** print_chessboard
- * takes in a 2D matrix
- * and represents it on the console
+
+/**
+ * print_chessboard - a function to print
+ * out the layout of the popular board game
+ * @a: consists of the flat view of a chess board
+ * Return: Doesn't return anything
  */
 
-void print_chessboard(char (*a)[8]){
-	
-	int i = 0;	
+void print_chessboard(char (*a)[8])
+{
+
+	int i = 0;
+	char *letter;
 
 	while (i < 8)
 	{
-		char *letter = a[i];	
+		letter = a[i];
 		write(1, letter, 8);
 		i++;
 		write(1, "\n", 1);
-	}	
+	}
 }
 
