29dd8f3a910f28657cc67e38a3447404c716689b
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-03 18:11:55 -0700

    Ragged Diagonal Between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_diagsums - a function to print out the values
+ * sum in a diagonal in regards to:
+ * @a: The diagonal array
+ * @size: The number of columns in the array
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0;
+	int sum2 = 0;
+	int position = 0;
+	int bound = size * size;
+	int going = size + 1;
+
+	while (position <= bound)
+	{
+		sum1 += *(a + position);
+		position += going;
+	}
+
+	position = bound - size;
+	going = size - 1;
+
+	while(position >= size - 1)
+	{
+		sum2 += *(a + position);		
+		position -= going;
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
+
