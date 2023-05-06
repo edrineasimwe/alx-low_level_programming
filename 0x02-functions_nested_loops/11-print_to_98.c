9e9fc3545502052e3f4ca7ffab2604ce2f67161b
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-06 04:31:35 -0700

    Modifications

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,18 +13,25 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d ", n);
+				printf("%d", n);
 				n--;
 				break;
 			}
 			printf("%d, ", n);
 			n--;
 		}
+		printf("\n");
 	}
 	else if (n < 98)
 	{
 		while (n <= 98)
 		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				n++;
+				break;
+			}
 			printf("%d, ", n);
 			n++;
 		}
