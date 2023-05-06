519a621ff61603adda2994e4697f16f528cb6ea8
edrineasimwe <edrineasiimwe12@gmail.com>
2023-05-06 06:55:21 -0700

    Diagonally parked in a parallel univers

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,7 +7,11 @@
 void print_diagonal(int n)
 {
 	int i, j;
-
+	
+	if (n < 1)
+	{
+		_putchar('\n');
+	}
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
@@ -17,6 +21,6 @@ void print_diagonal(int n)
 		_putchar(92);
 		_putchar('\n');
 	}
-	_putchar('\n');
+
 }
 
